1f4c207bae01943663b4bd19517354af0a549ba7
Tom Jones <list_lfa@yahoo.com>
2014-08-12 21:41:28 +1000

    a

--- src/Differ.java
+++ src/Differ.java
@@ -1,3 +1,11 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+
 /**
  * 
  */
@@ -10,10 +18,33 @@ public class Differ {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
+		
+		String input_file = "/home/a/differtest.png";
+		byte buf[] = new byte[1024];
+		int bytesRead = 0;
+		try {
+			DataInputStream dis = new DataInputStream(
+				    new BufferedInputStream(
+				        new FileInputStream(new File(input_file ))));
+			bytesRead = dis.read(buf);
+			System.out.format("bytes read [%d] \n", bytesRead);
+			System.out.println(Arrays.toString(buf));
+			System.out.format("0x%02X \n", buf[0]);
+			System.out.format("0x%02X \n", buf[1]);
+			System.out.format("0x%02X \n", buf[2]);
+			System.out.format("0x%02X \n", buf[3]);
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
